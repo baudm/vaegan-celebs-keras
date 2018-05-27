df0444524551f7c2fb47b34e1cb47b84d4866a33
PFValdez <16838175+echofox-pfv@users.noreply.github.com>
2018-05-27 20:25:53 +0800

    Add files via upload
    Adagrad as optimizer

---
+++ train_adagrad.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras.optimizers import Adagrad
+
+from vaegan.models import create_models, build_graph
+from vaegan.training import fit_models
+from vaegan.data import celeba_loader, encoder_loader, decoder_loader, discriminator_loader, NUM_SAMPLES, mnist_loader
+from vaegan.callbacks import DecoderOutputGenerator
+
+
+def set_trainable(model, trainable):
+    model.trainable = trainable
+    for layer in model.layers:
+        layer.trainable = trainable
+
+
+def main():
+    encoder, decoder, discriminator = create_models()
+    encoder_train, decoder_train, discriminator_train, vae, vaegan = build_graph(encoder, decoder, discriminator)
+
+    if len(sys.argv) == 3:
+        vaegan.load_weights(sys.argv[1])
+        initial_epoch = int(sys.argv[2])
+    else:
+        initial_epoch = 0
+
+    batch_size = 64
+
+    opt = Adagrad(lr=0.01, epsilon=None, decay=0.0)
+
+    set_trainable(encoder, False)
+    set_trainable(decoder, False)
+    discriminator_train.compile(opt, ['binary_crossentropy'] * 3, ['acc'] * 3)
+    discriminator_train.summary()
+
+    set_trainable(discriminator, False)
+    set_trainable(decoder, True)
+    decoder_train.compile(opt, ['binary_crossentropy'] * 2, ['acc'] * 2)
+    decoder_train.summary()
+
+    set_trainable(decoder, False)
+    set_trainable(encoder, True)
+    encoder_train.compile(opt)
+    encoder_train.summary()
+
+    set_trainable(vaegan, True)
+
+    checkpoint = ModelCheckpoint(os.path.join('.', 'model.{epoch:02d}.h5'), save_weights_only=True)
+    decoder_sampler = DecoderOutputGenerator()
+
+    callbacks = [checkpoint, decoder_sampler, TensorBoard()]
+
+    epochs = 100
+
+    steps_per_epoch = NUM_SAMPLES // batch_size
+
+    seed = np.random.randint(2**32 - 1)
+
+    img_loader = celeba_loader(batch_size, num_child=3, seed=seed)
+    dis_loader = discriminator_loader(img_loader, seed=seed)
+    dec_loader = decoder_loader(img_loader, seed=seed)
+    enc_loader = encoder_loader(img_loader)
+
+    models = [discriminator_train, decoder_train, encoder_train]
+    generators = [dis_loader, dec_loader, enc_loader]
+    metrics = [{'di_l': 1, 'di_l_t': 2, 'di_l_p': 3, 'di_a': 4, 'di_a_t': 7, 'di_a_p': 10}, {'de_l_t': 1, 'de_l_p': 2, 'de_a_t': 3, 'de_a_p': 5}, {'en_l': 0}]
+
+    histories = fit_models(vaegan, models, generators, metrics, batch_size,
+                           steps_per_epoch=steps_per_epoch, callbacks=callbacks,
+                           epochs=epochs, initial_epoch=initial_epoch)
+
+    vaegan.save_weights('trained.h5')
+
+    x = next(celeba_loader(1))
+
+    x_tilde = vae.predict(x)
+
+    plt.subplot(211)
+    plt.imshow((x[0].squeeze() + 1.) / 2.)
+
+    plt.subplot(212)
+    plt.imshow((x_tilde[0].squeeze() + 1.) / 2.)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
